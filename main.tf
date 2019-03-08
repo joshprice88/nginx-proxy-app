02302abad6852bad02d4ec0dac8bbe29a295e55b
josh <joshprice88@gmail.com>
2019-03-08 11:04:42 -0800

    2nd commit

--- main.tf
+++ main.tf
@@ -84,7 +84,154 @@ resource "aws_route_table_association" "private_assoc" {
 }
 
 
-#------Security Groups------
+#------Security Groups and Rules------
+
+resource "aws_security_group" "project_rp_sg" {
+	name = "project_rp_sg"
+	description = "Used for reverse proxy instance"
+	vpc_id = "${aws_vpc.project_vpc.id}"
+}
+
+resource "aws_security_group" "project_bastion_sg" {
+        name = "project_bastion_sg"
+        description = "Used for ssh access to bastion host"
+        vpc_id = "${aws_vpc.project_vpc.id}"
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+	type = "ingress"
+
+	from_port   = 80
+	to_port     = 80
+    	protocol    = "tcp"
+    	cidr_blocks = ["0.0.0.0/0"]
+	
+	security_group_id = "${aws_security_group.project_rp_sg.id}"
+
+}
+
+resource "aws_security_group_rule" "allow_ssh_inbound" {
+        type = "ingress"
+
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+
+        security_group_id = "${aws_security_group.project_bastion_sg.id}"
+
+}
+
+resource "aws_security_group_rule" "allow_ssh_from_bastion" {
+        type = "ingress"
+        
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        source_security_group_id = "${aws_security_group.project_bastion_sg.id}"
+        security_group_id = "${aws_security_group.project_rp_sg.id}"
+
+}
+
+resource "aws_security_group_rule" "allow_all_outbound_sg" {
+        type = "egress"
+
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+
+		cidr_blocks = ["0.0.0.0/0"]
+
+		security_group_id = "${aws_security_group.project_rp_sg.id}"
+
+}
+
+resource "aws_security_group_rule" "allow_all_outbound_bastion" {
+        type = "egress"
+
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+
+		cidr_blocks = ["0.0.0.0/0"]
+
+        security_group_id = "${aws_security_group.project_bastion_sg.id}"
+
+}
+
+#------Compute-----
+
+resource "aws_key_pair" "project_auth" {
+	key_name   = "${var.key_name}"
+	public_key = "${file(var.public_key_path)}"
+}
+
+resource "aws_instance" "nginx_rp" {
+	instance_type = "${var.nginx_instance_type}"
+	ami = "${var.nginx_instance_ami}"
+
+	tags {
+		Name = "project_nginx_rp"
+	}
+	
+	subnet_id = "${aws_subnet.public_subnets.*.id[0]}"
+	vpc_security_group_ids = ["${aws_security_group.project_rp_sg.id}"]
+	key_name = "${aws_key_pair.project_auth.id}"
+	
+	user_data = "${file(var.nginx_userdata_path)}"
+}
+
+resource "aws_instance" "nginx_bastion" {
+        instance_type = "${var.bastion_instance_type}"
+        ami = "${var.bastion_instance_ami}"
+
+        tags {
+                Name = "project_nginx_bastion"
+        }
+
+        subnet_id = "${aws_subnet.public_subnets.*.id[0]}"
+        vpc_security_group_ids = ["${aws_security_group.project_bastion_sg.id}"]
+	key_name = "${aws_key_pair.project_auth.id}"
+}
+	
+resource "aws_launch_configuration" "calibre_lc" {
+	name_prefix = "cal-lc-"
+	instance_type = "${var.calibre_instance_type}"
+	ami = "${var.calibre_instance_ami}"
+	security_groups = ["${aws_security_group.calibre_sg.id}"]
+	key_name = "${aws_key_pair.project_auth.id}"	
+	user_data = "${file(var.calibre_userdata_path)}"
+	
+	lifecycle {
+		create_before_destroy = true
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- vars.tf
+++ vars.tf
@@ -13,14 +13,26 @@ variable "public_subnet_zones" {
 	type = "list"
 }
 
-
 variable "private_subnet_names" {
         type = "list"
 }
 variable "private_subnet_cidrs" {
         type = "list"
 }
-
 variable "private_subnet_zones" {
         type = "list"
 }
+
+variable "key_name" {}
+variable "public_key_path" {}
+
+variable "nginx_instance_type" {}
+variable "nginx_instance_ami" {}
+variable "nginx_userdata_path" {}
+
+variable "bastion_instance_type" {}
+variable "bastion_instance_ami" {}
+
+variable "calibre_instance_type" {}
+variable "calibre_instance_ami" {}
+variable "calibre_userdata_path" {}
